f665b2cb8c0d81fb55edeca699906b4c66fd8023
le.van.thanh <lethanh9398@gmail.com>
2019-04-25 14:34:01 +0700

    config authen

---
+++ authenService/src/main/java/com/lethanh98/authen/config/entity/Token.java
@@ -0,0 +1,13 @@
+package com.lethanh98.authen.config.entity;
+
+public class Token {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ authenService/src/main/java/com/lethanh98/authen/config/entity/UserCredentials.java
@@ -0,0 +1,21 @@
+package com.lethanh98.authen.config.entity;
+
+public class UserCredentials {
+    private String username, password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
